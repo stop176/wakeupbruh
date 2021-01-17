0ce2aea4d8290dda2f78b0790af8c3fcf1d7fac9
stop176 <68391988+stop176@users.noreply.github.com>
2021-01-17 01:45:34 -0500

    eye aspect ratio
    added ratio

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import  "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from "react-webcam";
 import { ReactComponent as Logo } from './wakeupbruh.svg';
-
+//import math from "/.math";
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
   //  Load posenet
   const runFacemesh = async () => {
-    // OLD MODEL
-    //debugging
-
 
     // NEW MODEL
     const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
@@ -21,6 +18,18 @@ function App() {
       detect(net);
     }, 10);
   };
+
+  
+
+  const getPointDist = (eyePoint1, eyePoint2) => {
+
+    let xdiff = eyePoint1[0]-eyePoint2[0];
+    let ydiff = eyePoint1[1]-eyePoint2[1];
+    let squarex = Math.pow(xdiff,2);
+    let squarey = Math.pow(ydiff, 2);
+    return Math.sqrt(squarex+squarey);
+
+  }
 
   const detect = async (net) => {
     if (
@@ -43,11 +52,40 @@ function App() {
 
 
       const face = await net.estimateFaces({input:video});
-      console.log(face);
 
 
+      //console.log(face);
+      const eyeAspectRatio = () =>{
+      try {
+        let A = getPointDist(face[0].annotations.leftEyeUpper0[4], face[0].annotations.leftEyeLower0[5])
+        let B = getPointDist(face[0].annotations.leftEyeUpper0[3], face[0].annotations.leftEyeLower0[4])
+        let C = getPointDist(face[0].annotations.leftEyeUpper0[2], face[0].annotations.leftEyeLower0[3])
+        let wideLeft = getPointDist(face[0].annotations.leftEyeLower0[0], face[0].annotations.leftEyeLower0[8])
+
+        let D = getPointDist(face[0].annotations.rightEyeUpper0[2], face[0].annotations.rightEyeLower0[3])
+        let E = getPointDist(face[0].annotations.rightEyeUpper0[3], face[0].annotations.rightEyeLower0[4])
+        let F = getPointDist(face[0].annotations.rightEyeUpper0[4], face[0].annotations.rightEyeLower0[5])
+        let wideRight = getPointDist(face[0].annotations.rightEyeLower0[0], face[0].annotations.rightEyeLower0[8])
+    
+    
+       let leftRatio = (A+B+C)/(3.0+wideLeft);
+       let rightRatio = (D+E+F)/(3.0+wideRight);
+
+       return (leftRatio+rightRatio)/2;
+      } catch  {
+        return;
+      }
+       
+      }
+      try {
+        console.log(eyeAspectRatio());
+      } catch {
+        return;
+      }
     }
   };
+
+
 
 runFacemesh();
   return (
